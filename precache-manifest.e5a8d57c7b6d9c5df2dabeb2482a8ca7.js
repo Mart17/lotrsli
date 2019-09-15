010b9be2019f15562856464043045f2a56f2040f
Martin Suja <mart17@users.noreply.github.com>
2019-09-15 17:36:17 +0200

    Updates

---
+++ precache-manifest.e5a8d57c7b6d9c5df2dabeb2482a8ca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78d1048feecaa91256ae6ec15adfaa46",
+    "url": "/lotrsli/index.html"
+  },
+  {
+    "revision": "d82edd5d25722a8d441e",
+    "url": "/lotrsli/static/css/main.70978ff5.chunk.css"
+  },
+  {
+    "revision": "c9c53e4982eaf7ba6948",
+    "url": "/lotrsli/static/js/2.0440c2aa.chunk.js"
+  },
+  {
+    "revision": "d82edd5d25722a8d441e",
+    "url": "/lotrsli/static/js/main.a1b00b0f.chunk.js"
+  },
+  {
+    "revision": "ff9140de4386ba82cd8e",
+    "url": "/lotrsli/static/js/runtime~main.63f48616.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lotrsli/static/media/logo.5d5d9eef.svg"
+  }
+]);
