bb60f7fdcec9b72daa2ef00e56e8a154f43342c5
Martin Suja <mart17@users.noreply.github.com>
2019-09-17 16:53:09 +0200

    Use scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.scss'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faMusic, faCloudRain, faDove, faHatWizard, faFireAlt, faWind } from '@fortawesome/free-solid-svg-icons'
--- src/IconTable.js
+++ src/IconTable.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './App.scss'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -20,8 +20,8 @@ function IconTable() {
   }
 
   return (
-    <div className="table-responsive">
-      <table className="table">
+    <div className="Table-responsive">
+      <table className="Table">
         <tbody>
           <tr>
             {displayIcons(icons.row1)}
